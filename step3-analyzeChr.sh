a5fe3a16bccb7d7e130c155ccbba8e44663d6b7e
lcolladotor <lcollado@jhsph.edu>
2014-10-08 20:16:11 -0400

    Reduce initial required memory

--- step3-analyzeChr.sh
+++ step3-analyzeChr.sh
@@ -39,7 +39,7 @@ do
 #!/bin/bash
 #$ -cwd
 #$ -m e
-#$ -l mem_free=20G,h_vmem=34G,h_fsize=10G
+#$ -l mem_free=10G,h_vmem=34G,h_fsize=10G
 #$ -N ${sname}
 #$ -pe local ${CORES}
 #$ -hold_jid derMod-${EXPERIMENT}.${PREFIX}
