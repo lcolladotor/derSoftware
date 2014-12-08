b55d99e88eb518b386de32f24f46f8fb00183e55
lcolladotor <lcollado@jhsph.edu>
2014-12-08 13:51:19 -0500

    Use methods

--- simulation/bw/bigwig.sh
+++ simulation/bw/bigwig.sh
@@ -4,4 +4,4 @@
 #$ -N bigwig
 
 module load R/3.1.x
-Rscript -e "library('derfinder'); load('../CoverageInfo/fullCov.Rdata); createBw(fullCov, keepGR = FALSE); proc.time(); devtools::session_info()"
+Rscript -e "library('derfinder'); library('methods'); load('../CoverageInfo/fullCov.Rdata); createBw(fullCov, keepGR = FALSE); proc.time(); devtools::session_info()"
