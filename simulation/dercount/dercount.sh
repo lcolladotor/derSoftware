126c086414f4daa383007d4dff095684e2cca175
lcolladotor <lcollado@jhsph.edu>
2015-02-25 14:58:38 -0500

    Added script for creating the database used in derfinder-original

---
+++ simulation/dercount/dercount.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+## Based on a combination of
+# /amber2/scratch/jleek/orbFrontal/scripts/tophatCount.sh
+# /amber2/scratch/jleek/orbFrontal/scripts/count_all15.sh
+#
+## Most recently, based on:
+# /dcs01/stanley/work/brain_rna/dercount/dercount.sh
+
+# Directories
+MAINDIR=/dcs01/ajaffe/Brain/derRuns/derSoftware/simulation
+WDIR=${MAINDIR}/dercount
+DATADIR=${MAINDIR}/thout
+
+# A clone of https://github.com/alyssafrazee/derfinder
+# More specifically from https://github.com/lcolladotor/derfinder-original
+SDIR=/dcs01/ajaffe/Brain/derRuns/derfinder-original
+
+# Define variables
+SHORT='derC-sim'
+
+# Construct shell files
+cd ${DATADIR}
+for thout in sample*
+do
+	cd ${WDIR}
+	for chr in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y
+	do
+		echo "Creating script for sample ${thout} and chromosome ${chr}"
+		cat > ${WDIR}/.${thout}.${SHORT}.${chr}.sh <<EOF
+#!/bin/bash	
+echo "**** Job starts ****"
+date
+
+# Get the md5sum of the script I'm using
+md5sum ${SDIR}/countTophatReads.py
+
+# Create output directory 
+mkdir -p ${WDIR}/${thout}
+# Make logs directory
+mkdir -p ${WDIR}/${thout}/logs
+
+# run countTophatReads.py
+module load python/2.7.6
+python ${SDIR}/countTophatReads.py --file ${DATADIR}/${thout}/accepted_hits.bam --output ${WDIR}/${thout}/${chr}-bybp --kmer 101 --chrom ${chr}
+
+## Print the version of pysam used
+python -c "import pysam; print pysam.__version__"
+
+## Move log files into the logs directory
+mv ${thout}.${SHORT}.${chr}.* ${WDIR}/${thout}/logs/
+
+echo "**** Job ends ****"
+date
+EOF
+		call="qsub -cwd -l jabba,mem_free=30G,h_vmem=40G,h_fsize=20G -N ${thout}.${SHORT}.${chr} -m e ${WDIR}/.${thout}.${SHORT}.${chr}.sh"
+		echo $call
+		$call
+	done
+done
